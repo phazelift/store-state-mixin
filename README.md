213626ca51d89864c6d91ceae54eeec455e1cb1b
Dennis Raymondo van der Sluis <phazelift@gmail.com>
2016-06-04 01:18:11 +0300

    Hello World!

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# store-state-mixin
-A React mixin for adding (flux) stores state to a component and efficiently updating it
+<h3>store-state-mixin</h3>
+
+
+
+A React mixin for easily adding (flux) stores state to a component.
+
+- Efficiently updates component state, only for the store that changed
+- Installs listeners on componentDidMount
+- Removes listeners on componentWillUnmount
+- No dependencies
+- Made for Alt, but might be useful with some other React stores as well
+
+--
+
+**Usage**:
+
+
+>Install with npm: `npm install --save store-state-mixin`
+
+```javascript
+
+// require the mixin
+var StoreStateMixin= require( 'store-state-mixin' );
+
+// create a stores init object for the mixin, using key names that will be
+// set as keys in this component's state
+var stores= {
+	 someStore		: require( '../stores/someStore.js' )
+	,otherStore		: require( '../stores/otherStore.js' )
+};
+
+var MyTopLevelComponent= React.createClass({
+	
+	 // add stores state and updates to component by mixin
+	 mixins: [ StoreStateMixin(stores) ]
+
+	,render: function(){
+
+		// provide child components with stores state
+		return (
+			<div>
+				<SomeChildComponent {...this.state} />
+				<OtherChildComponent {...this.state} />
+			</div>
+		);
+	}
+});
+```
---
+++ StoreStateMixin.js
@@ -0,0 +1,61 @@
+//
+// store-state-mixin
+//
+//	A React mixin for easily adding (flux) stores state to a component
+//
+//
+// Copyright (c) 2015 Dennis Raymondo van der Sluis
+//
+// This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>
+//
+
+
+// a basic object map
+var map= ( obj, callback ) => {
+	var result= {};
+	Object.keys( obj ).forEach( (key) => result[key]= callback(obj[ key ], key) );
+ 	return result;
+};
+
+
+var StoreStateMixin= function( stores ){
+
+	return {
+
+		storeStateMixin_getState: function( name ){
+			var state= {};
+			state[ name ]= stores[ name ].getState();
+			return state;
+		}
+
+		,storeStateMixin_onChange: function( name ){
+			this.setState( this.storeStateMixin_getState(name) );
+		}
+
+		,getInitialState: function(){
+			return map( stores, (store) => store.getState() );
+		}
+
+		,componentDidMount: function(){
+			map( stores, (store, name) => store.listen(this.storeStateMixin_onChange.bind( this, name )) );
+		}
+
+		,componentWillUnmount: function(){
+			map( stores, (store, name) => store.unlisten(this.storeStateMixin_onChange.bind( this, name )) );
+		}
+	};
+
+};
+
+export default StoreStateMixin;
